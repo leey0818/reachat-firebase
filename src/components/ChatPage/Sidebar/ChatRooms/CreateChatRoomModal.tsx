f109bd5ff5053dcfe8732826c08603e8879f079e
leey0818 <leey0818@gmail.com>
2021-12-29 23:17:05 +0900

    채팅방 DB 등록 구현

--- src/components/ChatPage/Sidebar/ChatRooms/CreateChatRoomModal.tsx
+++ src/components/ChatPage/Sidebar/ChatRooms/CreateChatRoomModal.tsx
@@ -1,5 +1,7 @@
-import { Form, Input, Modal } from 'antd';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { Form, Input, message, Modal } from 'antd';
+import { getDatabase, push, ref, set } from 'firebase/database';
+import { getAuth, User } from 'firebase/auth';
 
 type CreateChatRoomModalProps = {
   visible: boolean;
@@ -12,8 +14,29 @@ type FormInputValues = {
   desc?: string;
 };
 
+const createChatRoom = (name: string, desc?: string) => {
+  const db = getDatabase();
+  const chatRoomsRef = ref(db, 'chatRooms');
+  const newRoomRef = push(chatRoomsRef);
+
+  const auth = getAuth();
+  const user = auth.currentUser as User;
+  const roomData = {
+    id: newRoomRef.key,
+    name: name,
+    description: desc || '',
+    createdBy: {
+      uid: user.uid,
+      time: new Date().toUTCString(),
+    },
+  };
+
+  return set(newRoomRef, roomData);
+};
+
 function CreateChatRoomModal(props: CreateChatRoomModalProps) {
   const [form] = Form.useForm();
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     form.resetFields();
@@ -21,12 +44,30 @@ function CreateChatRoomModal(props: CreateChatRoomModalProps) {
 
   const handleConfirm = () => form.submit();
   const handleSubmit = (values: FormInputValues) => {
-    console.log(values);
-    props.onCreated(values);
+    setLoading(true);
+
+    // 채팅방 생성
+    createChatRoom(values.name, values.desc)
+      .then(() => {
+        setLoading(false);
+        props.onCreated(values);
+      })
+      .catch((error) => {
+        setLoading(false);
+        message.error(error.message);
+      });
   };
 
   return (
-    <Modal title="채팅방 생성" forceRender={true} visible={props.visible} onCancel={props.onClose} onOk={handleConfirm}>
+    <Modal
+      title="채팅방 생성"
+      okText="생성"
+      confirmLoading={loading}
+      forceRender={true}
+      visible={props.visible}
+      onCancel={props.onClose}
+      onOk={handleConfirm}
+    >
       <Form layout="vertical" requiredMark="optional" form={form} onFinish={handleSubmit}>
         <Form.Item label="방 이름" name="name" rules={[{ required: true, message: '방 이름을 입력하세요.' }]}>
           <Input autoFocus />
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { ConfigProvider } from 'antd';
+import koKR from 'antd/lib/locale/ko_KR';
 import './index.css';
 import './firebase';
 import App from './App';
@@ -9,7 +11,9 @@ import store from './store';
 ReactDOM.render(
   <BrowserRouter>
     <Provider store={store}>
-      <App />
+      <ConfigProvider locale={koKR}>
+        <App />
+      </ConfigProvider>
     </Provider>
   </BrowserRouter>,
   document.getElementById('root')
